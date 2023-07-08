622455c48eaeeeeb4345addd91b58ab6c498d718
jldevia <jldevia81@gmail.com>
2023-07-08 17:36:44 -0300

    Se agregan ajustes para deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ const endpoints = {
   serviceOrders: `http://${host}/service_orders/`,
   employees: `http://${host}:3000/tracking-so/employees`,
   customers: `http://${host}:3000/tracking-so/customers`,
+  masterData: `http://${host}:8080/tracking-so/master-data`,
 };
 export const environment = {
   production: false,

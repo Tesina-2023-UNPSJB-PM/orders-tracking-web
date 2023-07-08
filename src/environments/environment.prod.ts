21d5b6ae2cb736f01616e163665c606311a11202
jldevia <jldevia81@gmail.com>
2023-07-08 19:55:05 -0300

    Ajustes en las url de la api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 const host = 'vps-3107443-x.dattaweb.com';
+const PREFIX_PATH = 'api';
+
 const endpoints = {
-  serviceOrders: `http://${host}/service_orders/`,
-  employees: `http://${host}:3000/tracking-so/employees`,
-  customers: `http://${host}:3000/tracking-so/customers`,
-  masterData: `http://${host}:8080/tracking-so/master-data`,
+  serviceOrders: `http://${host}/${PREFIX_PATH}/service_orders/`,
+  employees: `http://${host}/${PREFIX_PATH}/tracking-so/employees`,
+  customers: `http://${host}/${PREFIX_PATH}/tracking-so/customers`,
+  masterData: `http://${host}/${PREFIX_PATH}/tracking-so/master-data`,
 };
 export const environment = {
   production: false,

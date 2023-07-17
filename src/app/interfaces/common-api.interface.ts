e0ca62edbc30599f783dc8ee4eb6263985a6ace4
IsaiasArza <arza.isaias@gmail.com>
2023-07-16 21:14:49 -0300

    adding pagination on table

--- src/app/interfaces/common-api.interface.ts
+++ src/app/interfaces/common-api.interface.ts
@@ -1,7 +1,10 @@
 import { Observable } from 'rxjs';
+import { PageDto } from '../shared/pagination/page.dto';
+import { PageOptionsDto } from '../shared/pagination/page-options.dto';
 
 export interface CommonApi<T> {
   getAll?(): Observable<T[]>;
+  getPage?(pageOptionsDto: PageOptionsDto): Observable<PageDto<T>>;
   get?(): Observable<T>;
   add?(item: T): Observable<void>;
   save?(item: any): Observable<number>;

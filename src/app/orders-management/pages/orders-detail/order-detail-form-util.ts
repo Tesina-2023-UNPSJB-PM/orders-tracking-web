adf27de4b1c13c6290bcaa2b0afebffb0911a8e3
IsaiasArza <arza.isaias@gmail.com>
2023-10-08 21:12:05 -0300

    ready

--- src/app/orders-management/pages/orders-detail/order-detail-form-util.ts
+++ src/app/orders-management/pages/orders-detail/order-detail-form-util.ts
@@ -1,11 +1,11 @@
-import { FormBuilder, FormControl, FormGroup } from "@angular/forms"
-import { MasterDataCustomerDTO } from "src/app/dtos/master-data/master-data-customer.dto";
-import { MasterDataEmployeeDTO } from "src/app/dtos/master-data/master-data-employee.dto";
-import { MasterDataOrderPriorityDTO } from "src/app/dtos/master-data/master-data-order-priority.dto";
-import { MasterDataOrderStatusDTO } from "src/app/dtos/master-data/master-data-order-status.dto";
-import { MasterDataOrderTypeDTO } from "src/app/dtos/master-data/master-data-order-type.dto";
-import { SectorDTO } from "src/app/dtos/sector.dto";
-import { ServiceOrderDetailResponse } from "src/app/dtos/service-order-detail.dto";
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MasterDataCustomerDTO } from 'src/app/dtos/master-data/master-data-customer.dto';
+import { MasterDataEmployeeDTO } from 'src/app/dtos/master-data/master-data-employee.dto';
+import { MasterDataOrderPriorityDTO } from 'src/app/dtos/master-data/master-data-order-priority.dto';
+import { MasterDataOrderStatusDTO } from 'src/app/dtos/master-data/master-data-order-status.dto';
+import { MasterDataOrderTypeDTO } from 'src/app/dtos/master-data/master-data-order-type.dto';
+import { SectorDTO } from 'src/app/dtos/sector.dto';
+import { ServiceOrderDetailResponse } from 'src/app/dtos/service-order-detail.dto';
 
 export type OrderDetailFormType = FormGroup<{
   formBasic: FormGroup<{
@@ -32,19 +32,26 @@ export type OrderDetailFormType = FormGroup<{
     latitudeAddress: FormControl<string | null>;
     longitudeAddress: FormControl<string | null>;
     referenceInfo: FormControl<string | null>;
-  }>;}>;
+  }>;
+}>;
 
-export function createOrderDetailForm(builder: FormBuilder): OrderDetailFormType {
+export function createOrderDetailForm(
+  builder: FormBuilder
+): OrderDetailFormType {
   const valueStatusDefault = { code: 'PENDING', name: 'Pendiente' };
 
   return builder.group({
     formBasic: builder.group({
-      number: new FormControl<string | null>(''),
+      number: new FormControl<string | null>({
+        value: '',
+        disabled: true,
+      }),
       description: new FormControl<string | null>(''),
       type: new FormControl<MasterDataOrderTypeDTO | null>(null),
-      state: new FormControl<MasterDataOrderStatusDTO | null>(
-        valueStatusDefault
-      ),
+      state: new FormControl<MasterDataOrderStatusDTO | null>({
+        value: valueStatusDefault,
+        disabled: true,
+      }),
       priority: new FormControl<MasterDataOrderPriorityDTO | null>(null),
       customer: new FormControl<MasterDataCustomerDTO | null>(null),
     }),
@@ -89,7 +96,10 @@ export function createOrderDetailForm(builder: FormBuilder): OrderDetailFormType
   });
 }
 
-export function initializeFormData(form: OrderDetailFormType, data:ServiceOrderDetailResponse) {
+export function initializeFormData(
+  form: OrderDetailFormType,
+  data: ServiceOrderDetailResponse
+) {
   form.setValue({
     formBasic: {
       number: data?.number ?? null,
@@ -104,7 +114,7 @@ export function initializeFormData(form: OrderDetailFormType, data:ServiceOrderD
       sector: data.execution?.assignedSector ?? null,
       estimatedResolutionDate: null,
       estimatedResolutionTime: null,
-      observationsExecution: data.execution?.observations ?? null
+      observationsExecution: data.execution?.observations ?? null,
     },
     formLocation: {
       descriptionAddress: data?.destination?.address?.description ?? null,
@@ -114,26 +124,29 @@ export function initializeFormData(form: OrderDetailFormType, data:ServiceOrderD
       zipCodeAddress: data?.destination?.address?.zipCode ?? null,
       latitudeAddress: '' + data?.destination?.address?.latitude,
       longitudeAddress: '' + data?.destination?.address?.longitude,
-      referenceInfo: data?.destination?.referenceInfo ?? null
-    }
+      referenceInfo: data?.destination?.referenceInfo ?? null,
+    },
   });
 }
 
-function getMasterDataPriority(priority: string | undefined): MasterDataOrderPriorityDTO | null {
+function getMasterDataPriority(
+  priority: string | undefined
+): MasterDataOrderPriorityDTO | null {
   if (!priority) return null;
 
   switch (priority) {
     case 'LOW':
-      return {code: 'LOW', name : 'Baja'} as MasterDataOrderPriorityDTO;
+      return { code: 'LOW', name: 'Baja' } as MasterDataOrderPriorityDTO;
     case 'MEDIUM':
-      return {code: 'MEDIUM', name : 'Media'} as MasterDataOrderPriorityDTO;
+      return { code: 'MEDIUM', name: 'Media' } as MasterDataOrderPriorityDTO;
     case 'HIGH':
-      return {code: 'HIGH', name : 'Alta'} as MasterDataOrderPriorityDTO;
+      return { code: 'HIGH', name: 'Alta' } as MasterDataOrderPriorityDTO;
     default:
       return null;
   }
 }
-function getDateEstimatedResolution(estimatedResolutionTime: Date | undefined): Date | null {
-  throw new Error("Function not implemented.");
+function getDateEstimatedResolution(
+  estimatedResolutionTime: Date | undefined
+): Date | null {
+  throw new Error('Function not implemented.');
 }
-
